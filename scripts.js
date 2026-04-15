0f10cecfd7be485c83f44f0943f07dc6e9d8e9a2
Rogerio Fernandes <roferrock@gmail.com>
2026-04-14 21:24:10 -0300

    feat: persist tasks in localStorage

--- scripts.js
+++ scripts.js
@@ -31,6 +31,8 @@ const handleAddTask = () => {
   tasksContainer.appendChild(taskItemContainer);
 
   inputElement.value = "";
+
+  updateLocalStorage();
 };
 
 const handleClick = (taskContent) => {
@@ -41,6 +43,8 @@ const handleClick = (taskContent) => {
       task.firstChild.classList.toggle("completed");
     }
   }
+
+  updateLocalStorage();
 };
 
 const handleDeleteClick = (taskItemContainer, taskContent) => {
@@ -51,6 +55,8 @@ const handleDeleteClick = (taskItemContainer, taskContent) => {
       taskItemContainer.remove();
     }
   }
+
+  updateLocalStorage();
 }
 
 const handleInputChange = () => {
@@ -60,6 +66,50 @@ const handleInputChange = () => {
     return inputElement.classList.remove("error");
   }
 };
+
+const updateLocalStorage = () => {
+  const tasks = tasksContainer.childNodes;
+  const localStorageTasks = [ ...tasks ].map(task => {
+    const content = task.firstChild;
+    const isCompleted = content.classList.contains("completed");
+
+    return {description: content.innerText, isCompleted};
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(localStorageTasks));
+};
+
+const refreshTasksUsingLocalStorage = () => {
+  const tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+
+  if (!tasksFromLocalStorage) return;
+  
+  for (const task of tasksFromLocalStorage) {
+    const taskItemContainer = document.createElement("div");
+    taskItemContainer.classList.add("task-item");
+
+    const taskContent = document.createElement("p");
+    taskContent.innerText = task.description;
+
+    if (task.isCompleted) {
+      taskContent.classList.add("completed");
+    }
+
+    taskContent.addEventListener("click", () => handleClick(taskContent));
+
+    const deleteItem = document.createElement("i");
+    deleteItem.classList.add("far");
+    deleteItem.classList.add("fa-trash-alt");
+
+    deleteItem.addEventListener("click", () => handleDeleteClick(taskItemContainer, taskContent));
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+    tasksContainer.appendChild(taskItemContainer);
+  }
+};
+
+refreshTasksUsingLocalStorage();
 
 addTaskButton.addEventListener("click", () => handleAddTask());
 inputElement.addEventListener("input", () => handleInputChange());
